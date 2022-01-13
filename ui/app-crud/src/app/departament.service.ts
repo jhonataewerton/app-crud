d97cc9d213e9162e329527e730c50064458855b4
Jhonata Ewerton Marcos <jhonata.as@hotmail.com>
2022-01-13 14:51:47 -0300

    department list

--- ui/app-crud/src/app/departament.service.ts
+++ ui/app-crud/src/app/departament.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { Departament } from './departament';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DepartamentService {
+  readonly url = 'http://localhost:3000/departments';
 
-  constructor() { }
+  private departmentsSubjects$: BehaviorSubject<Departament[]> =
+    new BehaviorSubject<Departament[]>([]);
+  private loaded: boolean = false;
+  constructor(private http: HttpClient) {}
+
+  get(): Observable<Departament[]> {
+    if (!this.loaded) {
+      this.http
+        .get<Departament[]>(this.url)
+        .pipe(tap((deps) => console.log))
+        .subscribe(this.departmentsSubjects$);
+      this.loaded = true;
+    }
+    return this.departmentsSubjects$.asObservable();
+  }
+
+  add(d: Departament): Observable<Departament> {
+    return this.http
+      .post<Departament>(this.url, d)
+      .pipe(
+        tap((dep: Departament) => this.departmentsSubjects$.getValue().push(dep))
+      );
+  }
 }
--- ui/app-crud/src/app/departament/departament.component.ts
+++ ui/app-crud/src/app/departament/departament.component.ts
@@ -1,15 +1,53 @@
+import { DepartamentService } from './../departament.service';
+import { Departament } from './../departament';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-departament',
   templateUrl: './departament.component.html',
-  styleUrls: ['./departament.component.css']
+  styleUrls: ['./departament.component.css'],
 })
 export class DepartamentComponent implements OnInit {
-
-  constructor() { }
+  depName: string = '';
+  departments: Departament[] = [
+    { name: 'dep1', _id: 'asdf' },
+    { name: 'dep1', _id: 'asdf' },
+    { name: 'dep1', _id: 'asdf' },
+    { name: 'dep1', _id: 'asdf' },
+    { name: 'dep1', _id: 'asdf' },
+  ];
+  constructor(private departmentService: DepartamentService) {}
 
   ngOnInit(): void {
+    this.departmentService.get().subscribe({
+      next: (res) => {
+        this.departments = res;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
+  save() {
+    this.departmentService.add({ name: this.depName }).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.clearFields();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  clearFields() {
+    this.depName = '';
+  }
+
+  cancel() {}
+
+  delete(dep: Departament) {}
+
+  edit(dep: Departament) {}
 }
